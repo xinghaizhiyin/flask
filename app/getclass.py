afc8d79a657d71f9099ce4c773ec1da6878da0be
root <1562662129@qq.com>
2022-10-26 09:09:54 +0800

    更新

--- app/getclass.py
+++ app/getclass.py
@@ -1,6 +1,7 @@
 import sqlite3
 import re
 import requests
+from flask import request
 import json
 from requests_html import HTMLSession
 
@@ -78,10 +79,9 @@ def setvip():
     return json.dumps(list, ensure_ascii=False)
 
 
-def getdata():
+def getdata(id=1, address= '',arrange='',plot='',language='',year='',limit=''):
     session = HTMLSession()
-    # url = "https://www.freeok.vip/vodshow/{0}-{1}-{2}-{3}-{4}----{6}---{5}.html".format(id, address, plot, arrange, language, year,limit)
-    url = 'https://www.freeok.vip/vodshow/1-----------.html'
+    url = "https://www.freeok.vip/vodshow/{0}-{1}-{2}-{3}-{4}----{6}---{5}.html".format(id, address, plot, arrange, language, year,limit)
     h = session.get(url=url)
     pattern = 'original.*?referrerpolicy'
     data = re.findall(pattern, h.html.html)
@@ -117,9 +117,9 @@ def getdata():
 
 if __name__ == '__main__':
     # getdata()
-    # getvip(id=1)
+    getvip(id=1)
     # setvip()
-    print(getdata())
+    print(getvip(id=1))
 
 
 
