a0bac65af062aec34f47d2d2ada169f2ebbbbf38
root <1562662129@qq.com>
2022-10-27 04:16:03 +0800

    更新

--- app/getclass.py
+++ app/getclass.py
@@ -1,38 +1,38 @@
 import sqlite3
 import re
-import requests
+import time
+
+import requests,time
 from flask import request
 import json
 from requests_html import HTMLSession
 
 
-def getvip(id, address= '',arrange='',plot='',language='',year='',limit=''):
-    session = HTMLSession()
-    url = "https://www.freeok.vip/vodshow/{0}-{1}-{2}-{3}-{4}----{6}---{5}.html".format(id, address, plot, arrange, language, year,limit)
-    h = session.get(url=url)
-    pattern = 'original.*?referrerpolicy'
-    data = re.findall(pattern, h.html.html)
-    patternone = 'note.*?div'
-    patterntwo = '"/voddetail.*?title'
-    dataone = re.findall(patternone, h.html.html)
-    datatwo = re.findall(patterntwo, h.html.html)
-    id = 0
+def getvip(id=1):
     cx = sqlite3.connect("test.db")
-    # 删除表
+    # 删除表 animation movie tv
     # cx.execute('''
-    # DROP TABLE class;
+    # DROP TABLE animation;
     # ''')
     # 创建表
     # cx.execute('''
-    # create table class(
+    # create table animation(
     # img char(200),
     # name char(100),
     # icon char(100)
     # );''')
+    session = HTMLSession()
+    url = "https://www.freeok.vip/vodshow/{}-----------.html".format(id)
+    h = session.get(url=url)
+    print(url)
+    pattern = 'original.*?referrerpolicy'
+    data = re.findall(pattern, h.html.html)
+    patternone = 'note.*?div'
+    dataone = re.findall(patternone, h.html.html)
     list = []
     listone = []
     listtwo = []
-    cx.execute('Delete From class')
+
     # 获取图片和名称，并写入数据库
     for item in data:
         datatext = item[0:-16].replace("=", ":")
@@ -41,45 +41,65 @@ def getvip(id, address= '',arrange='',plot='',language='',year='',limit=''):
         datatext = datatext.replace(" ", ',')
         datatext = "{" + datatext + "}"
         datajson = json.loads(datatext)
-        list.append(datajson)
+        list.append(datajson['original'])
+        listone.append(datajson['alt'])
     # 获取影片记录，1080p，hd
     for itemone in dataone:
-        name = dict(zip(["name"], [itemone[6:-5]]))
-        listone.append(name)
-    # 获取跳转播放连接
-    for itemtwo in datatwo:
-        icon = dict(zip(["icon"], [itemtwo[1:-7]]))
-        listtwo.append(icon)
-    listsum = zip(list,listone,listtwo)
-    for item,item1,item2 in listsum:
+        listtwo.append(itemone[6:-5])
+    listsum = zip(list, listone, listtwo)
+    if id == 1:
+        cx.execute('Delete From movie')
+        for item, item1, item2 in listsum:
+            sql = 'insert into movie(img,name,icon) values("%s","%s","%s")' % (item, item1, item2)
+            cx.execute(sql)
+    elif id == 2:
+        cx.execute('Delete From tv')
+        for item, item1, item2 in listsum:
+            sql = 'insert into tv(img,name,icon) values("%s","%s","%s")' % (item, item1, item2)
+            cx.execute(sql)
+    elif id == 3:
+        cx.execute('Delete From animation')
+        for item, item1, item2 in listsum:
+            sql = 'insert into animation(img,name,icon) values("%s","%s","%s")' % (item, item1, item2)
+            cx.execute(sql)
         # 插入数据
         # sql = 'insert into class(id,img,name, icon) values("%s","%s","%s")' % (item, item1, item2)
         # 更新数据，有更新则替换，无则不需替换
-        sql = 'replace into class(img,name, icon) values("%s","%s","%s")' % (item, item1, item2)
+        # sql = 'replace into class(img,name, icon) values("%s","%s","%s")' % (item, item1, item2)
         # 删除数据
         # sql = 'Delete From class'
-        cx.execute(sql)
-        # cx.commit()
-    cursor = cx.execute("select * from class")
-    # 查询数据
-    listdata = []
-    for row in cursor:
-        listdata.append(row)
-    listdata = dict(zip(["data"], [listdata]))
-    return json.dumps(listdata, ensure_ascii=False)
+        # cx.execute(sql)
+    cx.commit()
 
-def setvip():
+
+def getsum():
+    getvip()
+    time.sleep(10)
+    getvip(2)
+    time.sleep(10)
+    getvip(3)
+
+
+def setvip(id=1):
     cx = sqlite3.connect("test.db")
-    cursor = cx.execute("select * from class")
+    if id == 1:
+        cursor = cx.execute("select * from movie")
+    elif id == 2:
+        cursor = cx.execute("select * from tv")
+    elif id == 3:
+        cursor = cx.execute("select * from animation")
     # 查询数据
     list = []
     for row in cursor:
-        list.append(row)
-    list = dict(zip(["data"], [list]))
+        img = dict(zip(["img"], [row[0]]))
+        icon = dict(zip(["icon"], [row[2]]), **img)
+        name = dict(zip(["name"], [row[1]]), **icon)
+        list.append(name)
+    # list = dict(zip(["data"], [list]))
     return json.dumps(list, ensure_ascii=False)
 
 
-def getdata(id=1, address= '',arrange='',plot='',language='',year='',limit=''):
+def getdata(id='', address='', arrange='', plot='', language='', year='', limit=''):
     session = HTMLSession()
     url = "https://www.freeok.vip/vodshow/{0}-{1}-{2}-{3}-{4}----{6}---{5}.html".format(id, address, plot, arrange, language, year,limit)
     h = session.get(url=url)
@@ -95,8 +115,8 @@ def getdata(id=1, address= '',arrange='',plot='',language='',year='',limit=''):
     # 处理数据，通过正则找出三个数据，然后添加到字典里面
     for item in data:
         datatext = item[0:-16].replace("=", ":")
-        datatext = datatext.replace("original", '"original"')
-        datatext = datatext.replace("alt", '"alt"')
+        datatext = datatext.replace("original", '"img"')
+        datatext = datatext.replace("alt", '"name"')
         datatext = datatext.replace(" ", ',')
         datatext = "{" + datatext + "}"
         datajson = json.loads(datatext)
@@ -107,8 +127,8 @@ def getdata(id=1, address= '',arrange='',plot='',language='',year='',limit=''):
         for itemtwo in datatwo:
             idname = itemtwo[1:-7]
             t.append(idname)
-        dataname = dict(zip(["name"], l))
-        idname = dict(zip(["icon"], t))
+        dataname = dict(zip(["icon"], l))
+        idname = dict(zip(["gethtml"], t))
         sum = dict(dataname,**datajson)
         list.append(dict(idname, **sum))
     # list = dict(zip(["data"], [list]))
@@ -116,10 +136,11 @@ def getdata(id=1, address= '',arrange='',plot='',language='',year='',limit=''):
 
 
 if __name__ == '__main__':
-    getdata()
-    # getvip(id=1)
+    # getdata()
+    # getvip(1)
     # setvip()
-    # print(getdata)
+    print(setvip(3))
+    print(getdata())
 
 
 
--- app/home.py
+++ app/home.py
@@ -6,6 +6,7 @@ from flask_apscheduler import APScheduler
 app = Flask(__name__)
 
 
+# 定时任务
 class Config(object):
     JOBS = [
         {
@@ -15,6 +16,15 @@ class Config(object):
             'day': '*',
             'hour': '22',
             'minute': '33',
+            'second': '20'
+        },
+        {
+            'id': 'job2',
+            'func': 'home:getclass.getsum',
+            'trigger': 'cron',
+            'day': '*',
+            'hour': '3',
+            'minute': '16',
             'second': '20'
         }
     ]
@@ -26,32 +36,44 @@ def hello_world():
     return data.setdata()
 
 
-@app.route('/films/vip', methods=["GET",'POST'])
+@app.route('/films/vip', methods=["GET", 'POST'])
 def vip():
+    id = int(request.values['id'])  # 类型
+    address = request.values['address']  # 地区
+    arrange = request.values['arrange']  # 类型
+    plot = request.values['plot']  # 排行
+    language = request.values['language']  # 语言
+    year = request.values['year']  # 年份
+    limit = request.values['limit']  # 翻页
     if request.method == "POST":
         # if request.content_type.startswith('application/json'):
         #     comment = request.json.get('content')
         # elif request.content_type.startswith('ultipart/for-data'):
         #     comment = request.form.get('content')
         # else:
-        id = request.values['id']  #类型
-        address = request.values['address']  #地区
-        arrange = request.values['arrange']  #类型
-        plot = request.values['plot']  #排行
-        language = request.values['language']  #语言
-        year = request.values['year']  #年份
-        limit = request.values['limit']  #翻页
-        datajson = getclass.getdata(id, address, arrange, plot, language, year, limit)
-    if request.method == "GET":
-        id = request.values['id']  #类型
-        address = request.values['address']  #地区
-        arrange = request.values['arrange']  #类型
-        plot = request.values['plot']  #排行
-        language = request.values['language']  #语言
-        year = request.values['year']  #年份
-        limit = request.values['limit']  #翻页
-        datajson = getclass.getdata(id, address, arrange, plot, language, year, limit)
-    return datajson
+        # 从数据库取
+        if address == '' and arrange == '' and plot == '' and language == '' and year == '' and limit == '':
+            if id == 1 :
+                return getclass.setvip(id)
+            elif id == 2:
+                return getclass.setvip(id)
+            elif id == 3:
+                return getclass.setvip(id)
+        else:
+            datajson = getclass.getdata(id, address, arrange, plot, language, year, limit)
+            return datajson
+    elif request.method == "GET":
+        # 从数据库取
+        if address == '' and arrange == ''and plot == ''and language == ''and year == ''and limit == '':
+            if id == 1:
+                return getclass.setvip(id)
+            elif id == 2:
+                return getclass.setvip(id)
+            elif id == 3:
+                return getclass.setvip(id)
+        else:
+            datajson = getclass.getdata(id, address, arrange, plot, language, year, limit)
+            return datajson
 
 
 if __name__ == '__main__':
